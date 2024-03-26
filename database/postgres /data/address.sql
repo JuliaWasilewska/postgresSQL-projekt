92d62086af9fbf07a71107a1764a35864930398f
JuliaWasilewska <121302186+JuliaWasilewska@users.noreply.github.com>
2024-03-26 13:21:57 +0100

    Create address.sql

---
+++ database/postgres /data/address.sql
@@ -0,0 +1,20 @@
+create table data.address
+(
+    id               serial
+        primary key,
+    street           text,
+    building_number  integer not null,
+    frame_number     integer,
+    apartment_number integer,
+    zip_code         text,
+    city             text,
+    country          text,
+    user_id          integer not null
+        unique
+        constraint user_id_fk
+            references data."user"
+            on update cascade on delete restrict
+);
+
+alter table data.address
+    owner to postgres;
