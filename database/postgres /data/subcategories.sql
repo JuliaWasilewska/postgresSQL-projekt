c7a8b90a111cf4680cf46972c6ef81af93d819ef
JuliaWasilewska <121302186+JuliaWasilewska@users.noreply.github.com>
2024-03-26 13:26:44 +0100

    Create subcategories.sql

---
+++ database/postgres /data/subcategories.sql
@@ -0,0 +1,14 @@
+create table data.subcategories
+(
+    id          serial
+        primary key,
+    subcategory text,
+    category_id integer not null
+        unique
+        constraint category_id_fk
+            references data.categories
+            on update cascade on delete restrict
+);
+
+alter table data.subcategories
+    owner to postgres;
